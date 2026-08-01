47bf9a3390b2f934d6c85da8284505fab1137a44
jojo58446tle-pixel <jojo58446tle@gmail.com>
2026-08-01 13:22:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://eknwvgjftjimurlynfmv.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://eknwvgjftjimurlynfmv.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_DWO4jdE2CBg2mV1ZkRX-OQ_ZLtbjTDf"
 };
